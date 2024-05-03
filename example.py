3149fe430c37ec97f8cb82a915c62945c75a760a
Ben Gardiner <ben.l.gardiner@gmail.com>
2024-05-03 00:22:47 -0400

    update python2 detection (six isn't always available everywhere it seems)

--- example.py
+++ example.py
@@ -4,6 +4,7 @@ import threading
 import time
 
 import can
+
 # scapy setup
 # # this works
 
@@ -13,29 +14,40 @@ import can
 # load_contrib('cansocket')
 
 # but this is more explicit
-import scapy.libs.six as six
 from scapy.config import conf
+
+try:
+    import scapy.libs.six as six
+
+    if six.PY2:
+        conf.contribs["CANSocket"] = {"use-python-can": True}
+except ModuleNotFoundError:
+    pass
 from scapy.consts import LINUX
+
+if not LINUX or conf.use_pypy:
+    conf.contribs["CANSocket"] = {"use-python-can": True}
+
 from scapy.scapypipes import WiresharkSink
-
 from scapy.sendrecv import sniff, AsyncSniffer
-
-if six.PY2 or not LINUX or conf.use_pypy:
-    conf.contribs['CANSocket'] = {'use-python-can': True}
-
 from scapy.contrib.cansocket import CANSocket, PYTHON_CAN  # noqa: E402
 from scapy.contrib.isotp import ISOTPSocket
 
 from scapy.contrib.automotive.uds import UDS
 
 from scapy.pipetool import CLIFeeder, ConsoleSink, PipeEngine
+
 # end scapy setup
 
 # logging setup
 from logging import getLogger, WARN, INFO, DEBUG, CRITICAL
 
-getLogger("scapy.contrib.automotive").setLevel(INFO)  # set to DEBUG e.g. for more logging
-getLogger("scapy.contrib.automotive.uds").setLevel(INFO)  # set to DEBUG e.g. for more logging
+getLogger("scapy.contrib.automotive").setLevel(
+    INFO
+)  # set to DEBUG e.g. for more logging
+getLogger("scapy.contrib.automotive.uds").setLevel(
+    INFO
+)  # set to DEBUG e.g. for more logging
 getLogger("scapy.contrib.isotp").setLevel(INFO)  # set to DEBUG e.g. for more logging
 
 # uncomment this for lightweight but kinda ugly logging
@@ -48,8 +60,10 @@ PYTHON_CAN_CHANNEL = "COM29"
 
 def candump_print_stderr(pkt, interface, channel):
     print(
-        f'({pkt.time:010.06f}) {interface}{channel} {pkt.identifier:03x}#{pkt.data.hex().ljust(18)}  ; {str(pkt.data)}',
-        file=sys.stderr, flush=True)
+        f"({pkt.time:010.06f}) {interface}{channel} {pkt.identifier:03x}#{pkt.data.hex().ljust(18)}  ; {str(pkt.data)}",
+        file=sys.stderr,
+        flush=True,
+    )
 
 
 feeder = CLIFeeder()
@@ -61,6 +75,7 @@ p = PipeEngine(feeder)
 p.start()
 time.sleep(6.0)  # wait for pipefeeder and wireshark to warm up
 
+
 def sniff_action(pkt, interface, channel):
     feeder.send(pkt)
     if wire.f is not None:
@@ -70,27 +85,33 @@ def sniff_action(pkt, interface, channel):
     return
 
 
-sniff_csock = CANSocket(bustype=PYTHON_CAN_INTERFACE, channel=PYTHON_CAN_CHANNEL, receive_own_messages=False)
+sniff_csock = CANSocket(
+    bustype=PYTHON_CAN_INTERFACE, channel=PYTHON_CAN_CHANNEL, receive_own_messages=False
+)
 
 sniffer_started = threading.Event()
-sniffer = AsyncSniffer(opened_socket=sniff_csock,
-                       prn=lambda pkt: sniff_action(pkt, PYTHON_CAN_INTERFACE, PYTHON_CAN_CHANNEL),
-                       store=0,
-                       started_callback=sniffer_started.set
-                       )
+sniffer = AsyncSniffer(
+    opened_socket=sniff_csock,
+    prn=lambda pkt: sniff_action(pkt, PYTHON_CAN_INTERFACE, PYTHON_CAN_CHANNEL),
+    store=0,
+    started_callback=sniffer_started.set,
+)
 sniffer.start()
 sniffer_started.wait(timeout=14.0)  # wait for sniffer to be running
 # end logging setup
 
-SEND_TO_ID = 0x7e1
+SEND_TO_ID = 0x7E1
 RECV_FR_ID = SEND_TO_ID + 8
 
 # example scapy automotive: ISOTP Send-Receive1. REPLACE THIS WITH YOUR SCRIPTS
 # ---
 
-csock = CANSocket(bustype=PYTHON_CAN_INTERFACE, channel=PYTHON_CAN_CHANNEL, receive_own_messages=False,
-                  can_filters=[{'can_id': RECV_FR_ID,
-                                'can_mask': 0x7FF}])  # set 'can_mask' 0x000 to pass all traffic; set to 0x7ff to pass only matched traffic
+csock = CANSocket(
+    bustype=PYTHON_CAN_INTERFACE,
+    channel=PYTHON_CAN_CHANNEL,
+    receive_own_messages=False,
+    can_filters=[{"can_id": RECV_FR_ID, "can_mask": 0x7FF}],
+)  # set 'can_mask' 0x000 to pass all traffic; set to 0x7ff to pass only matched traffic
 with ISOTPSocket(csock, tx_id=SEND_TO_ID, rx_id=RECV_FR_ID, basecls=UDS) as isock:
     resp = isock.sr1(UDS(service=0x33) / bytes([0x12]), timeout=14.0, retry=3)
     if resp is not None:
