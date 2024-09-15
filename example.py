4fde2e2e65d62469338adccd909289f577f0eee3
Ben Gardiner <ben.l.gardiner@gmail.com>
2024-09-15 17:28:50 +0100

    Update example.py
    * no wireshark by default
    * avoid using can filters on PEAK CAN 
    * more examples in the code

--- example.py
+++ example.py
@@ -30,7 +30,7 @@ if not LINUX or conf.use_pypy:
 
 from scapy.scapypipes import WiresharkSink
 from scapy.sendrecv import sniff, AsyncSniffer
-from scapy.contrib.cansocket import CANSocket, PYTHON_CAN  # noqa: E402
+from scapy.contrib.cansocket import CANSocket
 from scapy.contrib.isotp import ISOTPSocket
 
 from scapy.contrib.automotive.uds import UDS
@@ -55,7 +55,7 @@ getLogger("scapy.contrib.isotp").setLevel(INFO)  # set to DEBUG e.g. for more lo
 # that ^^^ can.bus.BusABC.RECV_LOGGING_LEVEL bodge _works_ but not great.
 
 PYTHON_CAN_INTERFACE = "slcan"
-PYTHON_CAN_CHANNEL = "COM29"
+PYTHON_CAN_CHANNEL = "COM8"
 PYTHON_CAN_BITRATE = 500_000
 
 
@@ -73,7 +73,7 @@ feeder > wire
 p = PipeEngine(feeder)
 
 # uncomment for CAN logging of the script in wireshark
-p.start()
+#p.start()
 time.sleep(6.0)  # wait for pipefeeder and wireshark to warm up
 
 
@@ -106,6 +106,9 @@ sniffer_started.wait(timeout=14.0)  # wait for sniffer to be running
 
 SEND_TO_ID = 0x7E1
 RECV_FR_ID = SEND_TO_ID + 8
+can_filters = None
+if PYTHON_CAN_INTERFACE != 'pcan':  # workaround python-can bug in pcan driver: can_filters disrupts timing too much for reliable ISO-TP
+    can_filters = [{'can_id': RECV_FR_ID, 'can_mask': 0x1FFFF}]
 
 # example scapy automotive: ISOTP Send-Receive1. REPLACE THIS WITH YOUR SCRIPTS
 # ---
@@ -115,10 +118,27 @@ csock = CANSocket(
     channel=PYTHON_CAN_CHANNEL,
     bitrate=PYTHON_CAN_BITRATE,
     receive_own_messages=False,
-    can_filters=[{"can_id": RECV_FR_ID, "can_mask": 0x1FFFF}],
+    can_filters=can_filters,
 )  # set 'can_mask' 0x000 to pass all traffic; set to 0x7ff to pass only matched traffic
 with ISOTPSocket(csock, tx_id=SEND_TO_ID, rx_id=RECV_FR_ID, basecls=UDS) as isock:
-    resp = isock.sr1(UDS(service=0x33) / bytes([0x12]), timeout=14.0, retry=3)
+    # there are a few examples of requests you could send here
+    # UDS()/UDS_DSC(diagnosticSessionType=0x3
+    # UDS()/UDS_SA(securityAccessType=0x1)
+    # UDS()/UDS_SA(securityAccessType=0x2, securityKey=b'SKEY')
+    # UDS()/UDS_WMBA(memorySizeLen=1, memoryAddressLen=4, memorySize1=2, memoryAddress4=0x0000057A, dataRecord=b'\x71\xcc')
+    #
+    # use these ones with ISOTPSocket(...,basecls=OBD) instead of =UDS above
+    # OBD()/OBD_S01(pid=[0x00])
+    # OBD()/OBD_S01(pid=[0x11]) # throttle position
+    # OBD()/OBD_S01(pid=[0x0c]) # engine speed
+    # OBD()/OBD_S09(iid=[0x0a]) # ecu name
+    # OBD()/OBD_S03()
+    #
+    # let's use this one by default
+    req = UDS(service=0x33) / bytes([0x12])  # a mysterious undocumented service
+    
+    resp = isock.sr1(req, timeout=14.0, retry=3)
+    
     if resp is not None:
         resp.display()
         print("response bytes: " + str(bytes(resp)))
